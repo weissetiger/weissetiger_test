dc265e0f0f093db44416ba5158dbc7bf85aae47d
Sebastian Ryu <sebastian.ryu@daumkakao.com>
2015-10-02 16:46:01 +0900

    create sample project

---
+++ src/main/java/org/weissetiger/WeissetigerApplication.java
@@ -0,0 +1,12 @@
+package org.weissetiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeissetigerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeissetigerApplication.class, args);
+    }
+}
